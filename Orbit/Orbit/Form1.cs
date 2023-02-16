686b1af17d36df74bc8bd5110fbb961ca8280ef8
JustOrbs <oliver.m.godwin@gmail.com>
2023-02-16 17:24:59 -0500

    Deleted some obselete methods: opening a localhost server, making a request for auth keys instead of launching the login page

--- Orbit/Orbit/Form1.cs
+++ Orbit/Orbit/Form1.cs
@@ -43,13 +43,6 @@ namespace Orbit
                 UseShellExecute = true,
             };
             Process.Start(getAuthKey);
-
-            //var client = new RestClient("https://www.bungie.net/en/oauth/authorize?response_type=code&client_id=40755&state=6i0mkLx79Hp91nzWVeHrzHG4");
-            //var request = new RestRequest();
-            //request.AddHeader("X-API-Key", "15f4cf036b0b4af9bc2338b2c1ef4ffc");
-            //request.AddHeader("Cookie", "Q6dA7j3mn3WPBQVV6Vru5CbQXv0q+I9ddZfGro+PognXQwjWM8PS+VE_=v1T9hRgw__r8l; __cflb=04dToX7HjFoF4QAzoaHehFaMj5fkjPQzpCaS7TUHC7; bungleanon=sv=BAAAAAC2QgAAAAAAAAu6KwEAAAAAAAAAAAAAAACQMy+tUp7aCEAAAADCYFR/ojplwdACRLFVE1iX6a48ZCkYrlZx+4lNMqLHpPCFS/mZ24NL0K9sVDdY3b52AsiMwMpGCWEYkWAJZwSt&cl=MC4xNzA3OC4xOTY0Mjg5MQ==; bungled=5591453895922266160; bungledid=B1KLq7vryaxDj1Mgm80E2YKCDC+tUp7aCAAA; bungles=WebView=False&UserFlowMode=SignIn&UserICT=None&UserSCT=None&UserForce=False&UserIDN=");
-            //var response = client.Execute(request);
-            //Console.WriteLine(response.Content);
         }
     }
 }
